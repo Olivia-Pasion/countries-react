38d15760d704a4f74d7958a987ff506c85266e22
olivia pasion <oap9820@gmail.com>
2022-09-19 17:30:38 -0700

    img element added to CountryCard.js, flags display with correlating country

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function CountryCard({ name }) {
+export default function CountryCard({ name, iso2 }) {
   return (
     <div className='country-card'>
       <div className='name'>{name}</div>
+      <div className='image'>
+        <img src={`https://flagcdn.com/72x54/${iso2.toLowerCase()}.png`} />
+      </div>
     </div>
   );
 }
